abf645a5242adfb942c514b5c0c312545ac31297
Uut Budiarto <fileku.uut@gmail.com>
2021-05-15 18:38:13 +0700

    membuat validation error

--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,13 +1,24 @@
+const { validationResult } = require("express-validator");
+
 exports.createBlog = (req, res, next) => {
   const title = req.body.title;
   // const image = req.body.image
   const body = req.body.body;
+
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const err = new Error("Invalid Value");
+    err.errorStatus = 400;
+    err.data = errors.array();
+    throw err;
+  }
+
   const result = {
     message: "Create Blog Success !",
     data: {
-      title: "data title",
+      title: title,
       image: "data-image-name.png",
-      body: "data body content blog",
+      body: body,
       created_at: "12/12/2021",
       author: {
         uid: 1,
@@ -15,5 +26,5 @@ exports.createBlog = (req, res, next) => {
       },
     },
   };
-  res.status(201).json(result)
+  res.status(201).json(result);
 };
--- src/routes/blog.js
+++ src/routes/blog.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const {body} = require('express-validator')
+
 const router = express.Router();
 const blogController = require('../controllers/blog');
 
-router.post('/create',blogController.createBlog)
+router.post('/create',[
+    body('title')
+    .isLength({min: 5}).withMessage('input minimal 5 karakter'),
+    body('body').isLength({min: 10}).withMessage('input minimal 10 karakter'),
+],blogController.createBlog)
 
 module.exports = router;
